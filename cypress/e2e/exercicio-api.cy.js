1a8cb16b852376be3891ad9201ecc386bbf4fcec
Lucas Jardim <lucasdovalle@hotmail.com>
2023-05-15 17:26:19 -0300

    Inlcusão do Jenkinsfile e correções

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -20,7 +20,7 @@ describe('Testes da Funcionalidade Usuários', () => {
                method: 'GET',
                url: 'usuarios'
           }).then((response) => {
-               expect(response.body.usuarios[2].nome).to.equal('Arnaldo Biruleibe')
+               expect(response.body.usuarios[0].nome).to.equal('Janira')
                expect(response.status).to.equal(200)
                expect(response.body).to.have.property('usuarios')
                expect(response.duration).to.be.lessThan(20)
